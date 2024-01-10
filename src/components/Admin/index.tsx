724355467f4125ee22602980683981578167caca
Mohit Rajan E <mohit.rajan@bigbinary.com>
2024-01-10 23:52:28 +0530

    view whitelisted user in UI

--- src/components/Admin/index.tsx
+++ src/components/Admin/index.tsx
@@ -1,9 +1,10 @@
 import { Routes, Route } from "react-router-dom";
+import WhitelistUsers from "./Whitelist";
 
 function AdminDashboard() {
   return (
     <Routes>
-      <Route path="/whitelist" element={<div>Whitlst</div>} />
+      <Route path="/whitelist" element={<WhitelistUsers />} />
       <Route path="/" element={<div>Admin panel</div>} />
     </Routes>
   );
