45f6e14bfdec626d84cb854067a6e49f6502ea4e
Mohit Rajan E <mohit.rajan@bigbinary.com>
2024-02-14 19:24:21 +0530

    Add staging domain to wellkown file

--- create_wellknown.sh
+++ create_wellknown.sh
@@ -3,10 +3,18 @@ mkdir dist/.well-known
 touch dist/.well-known/ic-domains
 touch dist/.ic-assets.json
 
-echo "dapp.elna.ai" > dist/.well-known/ic-domains
+if [ "$MODE" == "staging" ]; then
+  DOMAIN="beta.elna.ai"
+else
+  DOMAIN="dapp.elna.ai"
+fi
+
+echo "$DOMAIN" > dist/.well-known/ic-domains
 echo "[
   {
     \"match\": \".well-known\",
     \"ignore\": false
   }
 ]" > dist/.ic-assets.json
+
+echo "\n\n\n wellkown created. $DOMAIN\n\n\n"
