fb6b7585fd10ac6a45985c08c70cc0ad08f928d3
Mohit Rajan E <mohit.rajan@bigbinary.com>
2024-02-15 14:53:52 +0530

    update card design

--- src/components/CreateWizards/Card.tsx
+++ src/components/CreateWizards/Card.tsx
@@ -1,4 +1,3 @@
-import useGetDisplayAddress from "hooks/useGetDisplayAddress";
 import { Dropdown } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
@@ -11,6 +10,7 @@ interface CardProps {
   id: string;
   isPublished?: boolean;
   messagesReplied: bigint;
+  price?: number;
   handleDelete?: (id: string, name: string) => void;
   handlePublish?: (id: string, isPublished: boolean) => void;
 }
@@ -23,6 +23,7 @@ function Card({
   id,
   isPublished,
   messagesReplied,
+  price = 0,
   handleDelete,
   handlePublish,
 }: CardProps) {
@@ -44,28 +45,34 @@ function Card({
     <div className="col">
       <div className="card card-border contact-card elna-card">
         <div className="card-body text-center">
-          {!!handleDelete && (
-            <Dropdown className="card-body-menu">
-              <Dropdown.Toggle variant="dark" className="card-body-menu-button">
-                <i className="ri-more-line" />
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                <Dropdown.Item
-                  onClick={() => handleDelete(id, name)}
-                  className="card-dropdown-delete"
+          <div className="d-flex">
+            <div className="card-body__price">{!price ? "Free" : price}</div>
+            {!!handleDelete && (
+              <Dropdown className="card-body-menu">
+                <Dropdown.Toggle
+                  variant="dark"
+                  className="card-body-menu-button"
                 >
-                  {t("common.delete", { entity: "agent" })}
-                </Dropdown.Item>
-                {handlePublish && (
+                  <i className="ri-more-line" />
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
                   <Dropdown.Item
-                    onClick={() => handlePublish(id, !isPublished)}
+                    onClick={() => handleDelete(id, name)}
+                    className="card-dropdown-delete"
                   >
-                    {isPublished ? "Unpublish agent" : "Publish agent"}
+                    {t("common.delete", { entity: "agent" })}
                   </Dropdown.Item>
-                )}
-              </Dropdown.Menu>
-            </Dropdown>
-          )}
+                  {handlePublish && (
+                    <Dropdown.Item
+                      onClick={() => handlePublish(id, !isPublished)}
+                    >
+                      {isPublished ? "Unpublish agent" : "Publish agent"}
+                    </Dropdown.Item>
+                  )}
+                </Dropdown.Menu>
+              </Dropdown>
+            )}
+          </div>
           <div className="avatar avatar-xl avatar-rounded">
             {imageUrl ? (
               <img src={imageUrl} alt="user" className="avatar-img" />
@@ -80,13 +87,15 @@ function Card({
           </div>
           <div className="user-desg text-truncate">{description}</div>
         </div>
-        {userId && (
-          <div className="card-footer text-muted position-relative">
-            <a href="#" className="d-flex align-items-center p-0">
-              <span className="fs-7 lh-1">{displayAddress(userId)}</span>
-            </a>
+        <div className="d-flex card-footer position-relative align-items-center">
+          <div className="d-flex align-items-center gap-1">
+            <i className="ri-chat-4-line"></i>
+            <span>{messagesReplied.toString()}</span>
           </div>
-        )}
+          <span className="fs-7 ms-auto lh-1">
+            {userId && displayAddress(userId)}
+          </span>
+        </div>
       </div>
     </div>
   );
