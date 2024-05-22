6cda9327bb9a6d8b54f3f383cd874e3ace7986b8
Mohit Rajan E <mohit.rajan@bigbinary.com>
2024-05-22 15:31:02 +0530

    remove declarations from gitignore

---
+++ src/declarations/backend/backend.did.d.ts
@@ -0,0 +1,48 @@
+import type { Principal } from '@dfinity/principal';
+import type { ActorMethod } from '@dfinity/agent';
+import type { IDL } from '@dfinity/candid';
+
+export interface Backend {
+  'addAdmin' : ActorMethod<[Principal], string>,
+  'approvePendingDeveloper' : ActorMethod<[string], string>,
+  'generateUserToken' : ActorMethod<[], string>,
+  'getDevelopers' : ActorMethod<[], Array<Developer>>,
+  'getPendingDevelopers' : ActorMethod<[], Array<DeveloperApproval>>,
+  'getUserRequests' : ActorMethod<[], Array<DeveloperApproval>>,
+  'getUserToken' : ActorMethod<[Principal], string>,
+  'getWhitelistedUser' : ActorMethod<[], Array<Principal>>,
+  'isDeveloper' : ActorMethod<[], boolean>,
+  'isPrincipalAdmin' : ActorMethod<[Principal], boolean>,
+  'isUserAdmin' : ActorMethod<[], boolean>,
+  'isUserWhitelisted' : ActorMethod<[[] | [Principal]], boolean>,
+  'rejectPendingDeveloper' : ActorMethod<[string], string>,
+  'removeAdmin' : ActorMethod<[Principal], string>,
+  'removeWhitelistedUser' : ActorMethod<[Principal], string>,
+  'requestDeveloperAccess' : ActorMethod<[DeveloperApproval], boolean>,
+  'whitelistUser' : ActorMethod<[Principal], string>,
+}
+export interface Developer {
+  'id' : string,
+  'status' : DeveloperStatus,
+  'principal' : Principal,
+  'alias' : string,
+  'email' : string,
+  'github' : string,
+}
+export interface DeveloperApproval {
+  'id' : string,
+  'status' : DeveloperApprovalStatus,
+  'principal' : Principal,
+  'alias' : string,
+  'description' : string,
+  'email' : string,
+  'github' : string,
+}
+export type DeveloperApprovalStatus = { 'pending' : null } |
+  { 'approved' : null } |
+  { 'rejected' : null };
+export type DeveloperStatus = { 'disabled' : null } |
+  { 'approved' : null };
+export interface _SERVICE extends Backend {}
+export declare const idlFactory: IDL.InterfaceFactory;
+export declare const init: ({ IDL }: { IDL: IDL }) => IDL.Type[];
---
+++ src/declarations/backend/backend.did.js
@@ -0,0 +1,53 @@
+export const idlFactory = ({ IDL }) => {
+  const DeveloperStatus = IDL.Variant({
+    'disabled' : IDL.Null,
+    'approved' : IDL.Null,
+  });
+  const Developer = IDL.Record({
+    'id' : IDL.Text,
+    'status' : DeveloperStatus,
+    'principal' : IDL.Principal,
+    'alias' : IDL.Text,
+    'email' : IDL.Text,
+    'github' : IDL.Text,
+  });
+  const DeveloperApprovalStatus = IDL.Variant({
+    'pending' : IDL.Null,
+    'approved' : IDL.Null,
+    'rejected' : IDL.Null,
+  });
+  const DeveloperApproval = IDL.Record({
+    'id' : IDL.Text,
+    'status' : DeveloperApprovalStatus,
+    'principal' : IDL.Principal,
+    'alias' : IDL.Text,
+    'description' : IDL.Text,
+    'email' : IDL.Text,
+    'github' : IDL.Text,
+  });
+  const Backend = IDL.Service({
+    'addAdmin' : IDL.Func([IDL.Principal], [IDL.Text], []),
+    'approvePendingDeveloper' : IDL.Func([IDL.Text], [IDL.Text], []),
+    'generateUserToken' : IDL.Func([], [IDL.Text], []),
+    'getDevelopers' : IDL.Func([], [IDL.Vec(Developer)], ['query']),
+    'getPendingDevelopers' : IDL.Func([], [IDL.Vec(DeveloperApproval)], []),
+    'getUserRequests' : IDL.Func([], [IDL.Vec(DeveloperApproval)], ['query']),
+    'getUserToken' : IDL.Func([IDL.Principal], [IDL.Text], []),
+    'getWhitelistedUser' : IDL.Func([], [IDL.Vec(IDL.Principal)], []),
+    'isDeveloper' : IDL.Func([], [IDL.Bool], []),
+    'isPrincipalAdmin' : IDL.Func([IDL.Principal], [IDL.Bool], ['query']),
+    'isUserAdmin' : IDL.Func([], [IDL.Bool], ['query']),
+    'isUserWhitelisted' : IDL.Func(
+        [IDL.Opt(IDL.Principal)],
+        [IDL.Bool],
+        ['query'],
+      ),
+    'rejectPendingDeveloper' : IDL.Func([IDL.Text], [IDL.Text], []),
+    'removeAdmin' : IDL.Func([IDL.Principal], [IDL.Text], []),
+    'removeWhitelistedUser' : IDL.Func([IDL.Principal], [IDL.Text], []),
+    'requestDeveloperAccess' : IDL.Func([DeveloperApproval], [IDL.Bool], []),
+    'whitelistUser' : IDL.Func([IDL.Principal], [IDL.Text], []),
+  });
+  return Backend;
+};
+export const init = ({ IDL }) => { return [IDL.Principal]; };
