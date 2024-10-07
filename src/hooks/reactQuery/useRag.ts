c55c90e6ad420dc500e610f64c94c405b27ed7d9
Mohit Rajan E <mhit98@gmail.com>
2024-10-07 16:32:00 +0530

    send chat history (#183)
    * update rag canister
    
    * feat: send chat history to rag
    
    ---------
    
    Co-authored-by: Mohit Rajan E <mohit.rajan@bigbinary.com>

--- src/hooks/reactQuery/useRag.ts
+++ src/hooks/reactQuery/useRag.ts
@@ -3,16 +3,18 @@ import { v4 as uuidv4 } from "uuid";
 import { elna_RAG_backend as elnaRagBackend } from "declarations/elna_RAG_backend";
 import { QUERY_KEYS } from "src/constants/query";
 import { isErrGetFileNames } from "utils/ragCanister";
+import { History } from "declarations/elna_RAG_backend/elna_RAG_backend.did";
 
 type useChatPayload = {
   agentId: string;
   queryText: string;
   embeddings: [number];
+  history: History[];
 };
 export const useChat = () =>
   useMutation({
-    mutationFn: ({ agentId, queryText, embeddings }: useChatPayload) =>
-      elnaRagBackend.chat(agentId, queryText, embeddings, uuidv4()),
+    mutationFn: ({ agentId, queryText, embeddings, history }: useChatPayload) =>
+      elnaRagBackend.chat(agentId, queryText, embeddings, uuidv4(), history),
   });
 
 export const useGetFileNames = (agentId: string) =>
