0b96f6ed7b0ba5069fefcba5bd0b5ac60b7ae18f
Pratheesh Prasannakumar <hello@pratheesh.me>
2024-10-19 13:25:06 +0530

    Developer Profile Request Page UI Updated (#191)

--- vite.config.ts
+++ vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         "/api": {
-          target: "http://127.0.0.1:4943",
+          target: "https://icp0.io",
           changeOrigin: true,
         },
       },
