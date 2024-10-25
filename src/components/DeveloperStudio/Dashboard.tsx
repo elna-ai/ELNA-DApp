3749dc815b44718e2eb1b38ac43d16aae9c34e85
Pratheesh P <hello@pratheesh.me>
2024-10-25 10:34:04 +0530

    Listing tool Page UI Updated

--- src/components/DeveloperStudio/Dashboard.tsx
+++ src/components/DeveloperStudio/Dashboard.tsx
@@ -20,15 +20,7 @@ function Dashboard() {
   return (
     <div>
       {isDeveloper && (
-        <div
-          className="d-flex justify-content-between align-items-center p-2"
-          style={{
-            background:
-              "linear-gradient(272deg, #276E4C 49.81%, #266E4B 73.5%, #3D956A 97.18%)",
-            boxShadow: "0px 10px 20px 0px rgba(0, 0, 0, 0.08)",
-            borderRadius: "1rem",
-          }}
-        >
+        <div className="d-flex justify-content-between align-items-center list-tool">
           <div>
             <h5 className="mb-0">{t("developerStudio.createBanner.title")}</h5>
             <p className="mb-0" style={{ color: "#E0E0E1" }}>
