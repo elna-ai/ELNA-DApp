bdbbedac9e3001b51e7363ab83ca0bd6f01342b8
Pratheesh P <hello@pratheesh.me>
2024-11-27 10:27:57 +0530

    UI update in my space css

--- src/components/MySpace/Tools/index.tsx
+++ src/components/MySpace/Tools/index.tsx
@@ -13,62 +13,60 @@ import { Button } from "react-bootstrap";
 import ToolCard from "../../DeveloperStudio/ToolCard";
 
 function UserTools() {
-    const { t } = useTranslation();
-    const { data: isDeveloper, isFetching: isLoading } = useIsDeveloper();
-    const { data: userTools, isFetching: isUserToolsLoading } = useGetUserTools();
-    const navigate = useNavigate();
+  const { t } = useTranslation();
+  const { data: isDeveloper, isFetching: isLoading } = useIsDeveloper();
+  const { data: userTools, isFetching: isUserToolsLoading } = useGetUserTools();
+  const navigate = useNavigate();
 
-    const Title = () => {
-        return (
-            <div>
-                <div>
-                    <h2>Tools</h2>
-                    <h3>Each skilled......</h3>
-                </div>
-                {userTools && userTools?.length > 0 && (
-                    <Button
-                        disabled={!isDeveloper}
-                        onClick={() => navigate("my-space/my-tools/create-tool")}
-                    >
-                        {t("developerStudio.createBanner.listATool")}
-                    </Button>
-                )}
-            </div>
-        )
-    };
+  const Title = () => {
+    return (
+      <div>
+        <div>
+          <h5>Tools</h5>
+          <p>Each skilled......</p>
+        </div>
+        {userTools && userTools?.length > 0 && (
+          <Button
+            disabled={!isDeveloper}
+            onClick={() => navigate("my-space/my-tools/create-tool")}
+          >
+            {t("developerStudio.createBanner.listATool")}
+          </Button>
+        )}
+      </div>
+    );
+  };
 
-    const renderBody = () => {
-        
-        if (isUserToolsLoading) {
-            return <Spinner size="sm" />
-        }
-        
-        if (!isDeveloper) {
-            return <NoDeveloperAccess />
-        }
-    
-        if (userTools?.length === 0 || !userTools) {
-            return <NoTools />
-        }
-    
-        if(userTools?.length > 0 && !isUserToolsLoading) {
-            return (
-                <div className="d-flex flex-wrap gap-3">
-                {userTools?.map(tool => (
-                    <ToolCard tool={tool} key={tool.id} />
-                ))}
-            </div>
-            )
-        }
+  const renderBody = () => {
+    if (isUserToolsLoading) {
+      return <Spinner size="sm" />;
     }
 
+    if (!isDeveloper) {
+      return <NoDeveloperAccess />;
+    }
 
-    return (
-        <div>
-            <Title />
-            {renderBody()}
+    if (userTools?.length === 0 || !userTools) {
+      return <NoTools />;
+    }
+
+    if (userTools?.length > 0 && !isUserToolsLoading) {
+      return (
+        <div className="d-flex flex-wrap gap-3">
+          {userTools?.map(tool => (
+            <ToolCard tool={tool} key={tool.id} />
+          ))}
         </div>
-    );
+      );
+    }
+  };
+
+  return (
+    <div>
+      <Title />
+      {renderBody()}
+    </div>
+  );
 }
 
 export default UserTools;
