da78787a502a83347d9e6fe78be4317b862e3c02
Pratheesh P <hello@pratheesh.me>
2024-11-27 10:27:57 +0530

    UI update in my space css

--- src/components/MySpace/Agents/index.tsx
+++ src/components/MySpace/Agents/index.tsx
@@ -4,9 +4,9 @@ import Spinner from "react-bootstrap/Spinner";
 import { toast } from "react-toastify";
 import { useWallet } from "hooks/useWallet";
 import {
-    useDeleteMyWizard,
-    useFetchMyWizards,
-    usePublishUnpublishWizard,
+  useDeleteMyWizard,
+  useFetchMyWizards,
+  usePublishUnpublishWizard,
 } from "hooks/reactQuery/wizards/useMyWizards";
 import { useGetAllAnalytics } from "hooks/reactQuery/wizards/useAnalytics";
 import CheckWizardNameCreateModal from "components/common/CheckWizardNameCreate";
@@ -19,141 +19,142 @@ import { useNavigate } from "react-router-dom";
 import { useDeleteCollections } from "hooks/reactQuery/useRag";
 
 function MyWizards() {
-    const [isDeleteWizard, setIsDeleteWizard] = useState(false);
-    const [wizardToDelete, setWizardIdToDelete] = useState<{
-        id: string;
-        name: string;
-    }>();
+  const [isDeleteWizard, setIsDeleteWizard] = useState(false);
+  const [wizardToDelete, setWizardIdToDelete] = useState<{
+    id: string;
+    name: string;
+  }>();
 
-    const wallet = useWallet();
-    const navigate = useNavigate();
+  const wallet = useWallet();
+  const navigate = useNavigate();
 
-    const {
-        data: userWizards,
-        isFetching: isUserWizardsLoading,
-        isError,
-        error,
-    } = useFetchMyWizards({
-        userId: wallet?.principalId,
-    });
-    const { mutate: deleteMyWizard, isPending: isDeletePending } =
-        useDeleteMyWizard();
-    const { mutate: publishUnpublishWizard } = usePublishUnpublishWizard();
-    const { mutate: deleteIndex } = useDeleteCollections();
-    const { data: analytics } = useGetAllAnalytics();
+  const {
+    data: userWizards,
+    isFetching: isUserWizardsLoading,
+    isError,
+    error,
+  } = useFetchMyWizards({
+    userId: wallet?.principalId,
+  });
+  const { mutate: deleteMyWizard, isPending: isDeletePending } =
+    useDeleteMyWizard();
+  const { mutate: publishUnpublishWizard } = usePublishUnpublishWizard();
+  const { mutate: deleteIndex } = useDeleteCollections();
+  const { data: analytics } = useGetAllAnalytics();
 
-    const handleDeletePopup = (id: string, name: string) => {
-        setIsDeleteWizard(true);
-        setWizardIdToDelete({ id, name });
-    };
+  const handleDeletePopup = (id: string, name: string) => {
+    setIsDeleteWizard(true);
+    setWizardIdToDelete({ id, name });
+  };
 
-    const handleDelete = async (id: string) => {
-        deleteMyWizard(
-            { wizardId: id },
-            {
-                onError: e => {
-                    toast.error("Unable to delete agent");
-                    console.error(e);
-                },
-                onSettled: () => {
-                    setWizardIdToDelete(undefined);
-                    setIsDeleteWizard(false);
-                },
-                onSuccess: () => {
-                    deleteIndex(id);
-                },
-            }
-        );
-    };
+  const handleDelete = async (id: string) => {
+    deleteMyWizard(
+      { wizardId: id },
+      {
+        onError: e => {
+          toast.error("Unable to delete agent");
+          console.error(e);
+        },
+        onSettled: () => {
+          setWizardIdToDelete(undefined);
+          setIsDeleteWizard(false);
+        },
+        onSuccess: () => {
+          deleteIndex(id);
+        },
+      }
+    );
+  };
 
-    const handleOnHide = () => {
-        if (isDeletePending) return;
+  const handleOnHide = () => {
+    if (isDeletePending) return;
 
-        setWizardIdToDelete(undefined);
-        setIsDeleteWizard(false);
-    };
+    setWizardIdToDelete(undefined);
+    setIsDeleteWizard(false);
+  };
 
-    const handleEdit = (id: string) => {
-        navigate(`/create-agent/edit/${id}`);
-    };
+  const handleEdit = (id: string) => {
+    navigate(`/create-agent/edit/${id}`);
+  };
 
-    const Title = () => {
-        return (
-            <div>
-                <div>
-                    <h2>Agents</h2>
-                    <h3>Each skilled......</h3>
-                </div>
-                {
-                    !isUserWizardsLoading && ((userWizards?.length || 0) > 0) &&
-                    <CheckWizardNameCreateModal>
-                        <Button>Create Agent</Button>
-                    </CheckWizardNameCreateModal>
-                }
-            </div>
-        )
-    };
+  const Title = () => {
+    return (
+      <div>
+        <div>
+          <h5>Agents</h5>
+          <p>
+            Each skilled in their own field - a Travel Guide, a Fitness Coach, a
+            Masterful Storyteller, et al.
+          </p>
+        </div>
+        {!isUserWizardsLoading && (userWizards?.length || 0) > 0 && (
+          <CheckWizardNameCreateModal>
+            <Button>Create Agent</Button>
+          </CheckWizardNameCreateModal>
+        )}
+      </div>
+    );
+  };
 
-    // TODO: Refactor
-    const renderBody = () => {
-        if (isUserWizardsLoading) {
-            return <Spinner className="!flex mx-auto" />;
-        }
+  // TODO: Refactor
+  const renderBody = () => {
+    if (isUserWizardsLoading) {
+      return <Spinner className="!flex mx-auto" />;
+    }
 
-        if ((userWizards?.length || 0) === 0) {
-            return <NoWizards />;
-        }
-
-        return (
-            <>
-                <div className="my-wizards__card-wrapper">
-                    {userWizards?.map(
-                        ({ id, name, description, avatar, isPublished, creatorName }) => (
-                            <div key={id} className="col">
-                                <Card
-                                    name={name}
-                                    description={description}
-                                    id={id}
-                                    isPublished={isPublished}
-                                    handlePublish={(id, shouldPublish) =>
-                                        publishUnpublishWizard({ wizardId: id, shouldPublish })
-                                    }
-                                    imageId={avatar}
-                                    handleDelete={handleDeletePopup}
-                                    messagesReplied={analytics?.[id]?.messagesReplied || 0n}
-                                    handleEdit={handleEdit}
-                                    creatorName={creatorName}
-                                />
-                            </div>
-                        )
-                    )}
-                </div>
-            </>
-        );
-
-    };
-
-    useEffect(() => {
-        if (!isError) return;
-
-        toast.error(error.message);
-    }, [isError]);
+    if ((userWizards?.length || 0) === 0) {
+      return <NoWizards />;
+    }
 
     return (
-        <div className="">
-            <Title />
-            <div className="">
-                {renderBody()}
-                <DeleteWizardModal
-                    isDeleting={isDeletePending}
-                    isOpen={isDeleteWizard}
-                    wizardName={wizardToDelete?.name}
-                    onHide={handleOnHide}
-                    handleDelete={() => handleDelete(wizardToDelete!.id)}
+      <>
+        <div className="my-wizards__card-wrapper">
+          {userWizards?.map(
+            ({ id, name, description, avatar, isPublished, creatorName }) => (
+              <div key={id} className="col">
+                <Card
+                  name={name}
+                  description={description}
+                  id={id}
+                  isPublished={isPublished}
+                  handlePublish={(id, shouldPublish) =>
+                    publishUnpublishWizard({ wizardId: id, shouldPublish })
+                  }
+                  imageId={avatar}
+                  handleDelete={handleDeletePopup}
+                  messagesReplied={analytics?.[id]?.messagesReplied || 0n}
+                  handleEdit={handleEdit}
+                  creatorName={creatorName}
                 />
-            </div>
+              </div>
+            )
+          )}
         </div>
+      </>
     );
+  };
+
+  useEffect(() => {
+    if (!isError) return;
+
+    toast.error(error.message);
+  }, [isError]);
+
+  return (
+    <div className="">
+      <Title />
+      <div className="">
+        {renderBody()}
+        <DeleteWizardModal
+          isDeleting={isDeletePending}
+          isOpen={isDeleteWizard}
+          wizardName={wizardToDelete?.name}
+          onHide={handleOnHide}
+          handleDelete={() => handleDelete(wizardToDelete!.id)}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default MyWizards;
