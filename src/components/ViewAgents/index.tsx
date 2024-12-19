6d85674a0889991f2d5c72e88301f02ddb20201f
Rishikesh Dasappa <rishikeshdasappa98@gmail.com>
2024-12-20 00:41:35 +0530

    First hook use + en json init

--- src/components/ViewAgents/index.tsx
+++ src/components/ViewAgents/index.tsx
@@ -1,16 +1,25 @@
 import WizardPlaceholder from "./WizardPlaceholder";
 import PopularWizards from "./PopularWizards";
 import { useGetAllAnalytics } from "hooks/reactQuery/wizards/useAnalytics";
+import MetaTags from "components/common/MetaHelmet";
+import { useTranslation } from "react-i18next";
 
 function CreateWizards() {
   // To preload all the analytics data
   useGetAllAnalytics();
+  const { t } = useTranslation();
 
   return (
-    <div className="w-100">
-      <WizardPlaceholder />
-      <PopularWizards isHomePage={true} />
-    </div>
+    <>
+      <MetaTags
+        title={t("meta.home.title")}
+        description={t("meta.home.description")}
+      />
+      <div className="w-100">
+        <WizardPlaceholder />
+        <PopularWizards isHomePage={true} />
+      </div>
+    </>
   );
 }
 
