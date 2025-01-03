7e6d983037d31721c461109e1f218094ed36a9f5
Rishikesh Dasappa <rishikeshdasappa98@gmail.com>
2025-01-03 17:11:14 +0530

    content add + script section frame

--- src/components/ViewAgents/WidgetIntegration/InstallTypes.tsx
+++ src/components/ViewAgents/WidgetIntegration/InstallTypes.tsx
@@ -1,6 +1,6 @@
 import { InputGroup, FormControl, Button } from "react-bootstrap";
 import { copyToClipBoard } from "utils/index";
-import { PACKAGE_INSTALL_EXAMPLE, PACKAGE_INSTALL_README } from "./constant";
+import { PACKAGE_INSTALL_EXAMPLE, PACKAGE_INSTALL_README, SCRIPT_INSTALL_EXAMPLE, SCRIPT_INSTALL_README } from "./constant";
 
 export default function PackageInstall() {
     return (
@@ -41,7 +41,38 @@ export default function PackageInstall() {
 
 export function ScriptInstall() {
     return (
-        <div>ScriptInstall</div>
+        <div>
+            <div>Add the following Script tag to your HTML page</div>
+            <InputGroup className="mb-3 d-flex flex-column">
+                {
+                    SCRIPT_INSTALL_README.map((item) => (
+                        <>
+                            <label htmlFor={item.command}>{item.command}</label>
+                            <div className="d-flex">
+                                <FormControl name={item.command} value={item.installCommand} readOnly />
+                                <Button
+                                    onClick={() => copyToClipBoard(item.command, item.installCommand)}
+                                    className="position-absolute end-0" variant="outline-secondary"
+                                >
+                                    <i className="ri-file-copy-line"></i>
+                                </Button>
+                            </div>
+                        </>
+                    ))
+                }
+                <div>Widget Integration</div>
+                <label htmlFor="package-install-example">Example</label>
+                <div className="d-flex">
+                    <textarea readOnly name="package-install-example" value={SCRIPT_INSTALL_EXAMPLE("AGENT_UUID_IN_ELNA", "LOGO_URL")} />
+                    <Button
+                        onClick={() => copyToClipBoard("Example", SCRIPT_INSTALL_EXAMPLE("AGENT_UUID_IN_ELNA", "LOGO_URL"))}
+                        className="position-absolute end-0" variant="outline-secondary"
+                    >
+                        <i className="ri-file-copy-line"></i>
+                    </Button>
+                </div>
+            </InputGroup>
+        </div>
     )
 }
 
--- src/components/ViewAgents/WidgetIntegration/constant.ts
+++ src/components/ViewAgents/WidgetIntegration/constant.ts
@@ -17,3 +17,27 @@ export const PACKAGE_INSTALL_EXAMPLE = (
       ></elna-chat-widget>
 `;
 };
+
+export const SCRIPT_INSTALL_README = [
+  {
+    command: "Script",
+    installCommand:
+      "https://cdn.jsdelivr.net/npm/@elna-ai/chat-widget@2.0.3/+esm",
+  },
+];
+
+export const SCRIPT_INSTALL_EXAMPLE = (
+  AGENT_UUID_IN_ELNA: string,
+  LOGO_URL: string
+) => {
+  return `<script type="module">
+      import "https://cdn.jsdelivr.net/npm/@elna-ai/chat-widget@2.0.3/+esm";
+    </script>
+     ...
+    <elna-chat-widget
+        agentId="${AGENT_UUID_IN_ELNA}"
+        logo="${LOGO_URL}"
+        headerBackgroundColor="#{VALID_VALUE_FOR_COLOR_PROPERTY}"
+        ></elna-chat-widget>
+  `;
+};
