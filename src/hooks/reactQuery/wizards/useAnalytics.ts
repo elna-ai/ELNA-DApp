82f191bcbbfed013be17ea911beb079e3b9827f3
Mohit Rajan E <mhit98@gmail.com>
2025-02-12 19:04:43 +0530

    feat: add uniqueUsers and modificationCount to analytics

--- src/hooks/reactQuery/wizards/useAnalytics.ts
+++ src/hooks/reactQuery/wizards/useAnalytics.ts
@@ -1,5 +1,10 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { wizard_details as wizardDetails } from "declarations/wizard_details";
+import { Analytics_V2_External } from "declarations/wizard_details/wizard_details.did";
+import { useWallet } from "hooks/useWallet";
+import { canisterId, idlFactory } from "declarations/wizard_details";
+import { Main } from "declarations/wizard_details/wizard_details.did";
+
 import { ONE_HOUR_STALE_TIME, QUERY_KEYS } from "src/constants/query";
 
 export const useGetAllAnalytics = () =>
@@ -7,23 +12,37 @@ export const useGetAllAnalytics = () =>
     queryKey: [QUERY_KEYS.ALL_ANALYTICS],
     queryFn: () => wizardDetails.getAllAnalytics(),
     select: data => {
-      const analytics: { [key: string]: { messagesReplied: bigint } } = {};
+      const analytics: { [key: string]: Analytics_V2_External } = {};
       if (data?.length === 0) {
         return analytics;
       }
 
       data.forEach(analytic => {
-        analytics[analytic[0]] = analytic[1].v1;
+        analytics[analytic[0]] = analytic[1];
       });
       return analytics;
     },
     staleTime: ONE_HOUR_STALE_TIME,
   });
 
-export const useUpdateMessagesReplied = () =>
-  useMutation({
+export const useUpdateMessagesReplied = () => {
+  const wallet = useWallet();
+
+  return useMutation({
     mutationKey: ["update-messages-replied"],
-    mutationFn: (wizardId: string) =>
-      wizardDetails.updateMessageAnalytics(wizardId),
+    mutationFn: async (wizardId: string) => {
+      if (wallet === undefined) {
+        throw Error("User not logged in");
+      }
+
+      const wizardDetails: Main = await wallet.getCanisterActor(
+        canisterId,
+        idlFactory,
+        false
+      );
+      const response = await wizardDetails.updateMessageAnalytics(wizardId);
+      return response;
+    },
     onError: error => console.error(error.message),
   });
+};
