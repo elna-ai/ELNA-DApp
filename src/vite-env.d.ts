cae0335c3f41bc8173868fee3cf9c2f764dd4944
Mohit Rajan E <mhit98@gmail.com>
2025-02-19 10:16:10 +0530

    housekeeping:add typescript complete for env variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_EXTERNAL_SERVICE_BASE: string;
+  readonly VITE_ELNA_RAG_CANISTER_ID: string;
+  readonly VITE_ELNA_IMAGE_CANISTER_ID: string;
+  readonly VITE_INTEGRATIONS_BASE: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
